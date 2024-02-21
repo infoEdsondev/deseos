0744cc9628e9dfd2ef1693f98f3c08a9b1982952
eddofr <103290823+eddofr@users.noreply.github.com>
2024-02-21 16:28:07 -0500

    commit con lo ultimo de la app validado la persistencia en localstorage

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { DeseosService } from 'src/app/services/deseos.service';
 
 @Component({
@@ -10,12 +11,44 @@ import { DeseosService } from 'src/app/services/deseos.service';
 export class Tab1Page {
 
   constructor( public deseosService : DeseosService,
-               private router : Router){
+               private router : Router,
+               private alertController : AlertController){
     
   }
 
-  agregarLista(){
-    this.router.navigateByUrl('/tabs/tab1/agregar');
+  async agregarLista(){
+    //this.router.navigateByUrl('/tabs/tab1/agregar');
+    const alert = await this.alertController.create({
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name : 'titulo', 
+          type : 'text',
+          placeholder : 'Nombre de la Lista'
+        }
+      ],
+      buttons: [
+        {
+          text : 'Cancelar',
+          role : 'cancel',
+          handler : ()=>{
+            console.log('cancelado');
+          }
+        },
+        {
+          text : 'Crear',
+          handler : (data)=>{
+            console.log(data.titulo);
+            if(data.titulo.length===0){
+              return;
+            }
+            this.deseosService.crearLista(data.titulo);
+          }
+        }
+      ],
+    });
+
+    await alert.present();
   }
 
 }
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,9 +9,28 @@ export class DeseosService {
   listas : Lista[] = [];
 
   constructor() { 
-    const lista1 = new Lista('Recolectar piedras del infinito');
-    const lista2 = new Lista('Heroes a desaparecer');
-    this.listas.push(lista1, lista2);
-    console.log(this.listas); 
+    //const lista1 = new Lista('Recolectar piedras del infinito');
+    //const lista2 = new Lista('Heroes a desaparecer');
+    //this.listas.push(lista1, lista2);
+    //console.log(this.listas); 
+    this.cargarStorage();
   }
+
+  crearLista(titulo : string){
+    let nuevalista = new Lista(titulo);
+    this.listas.push(nuevalista);
+    this.guardarStorage();
+  }
+
+  cargarStorage(){
+    if( localStorage.getItem('data') ){
+      this.listas = JSON.parse(localStorage.getItem('data')!);
+    }
+    
+  }
+
+  guardarStorage(){
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
 }
